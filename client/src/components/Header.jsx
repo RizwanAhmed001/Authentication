86bf893529e430da5dbf0ded31d213a7355b907c
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2025-09-01 12:20:10 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { assets } from "../assets/assets";
-import { AppContext } from "../context/appcontext";
+import { AppContext } from "../context/Appcontext";
 
 function Header() {
 
